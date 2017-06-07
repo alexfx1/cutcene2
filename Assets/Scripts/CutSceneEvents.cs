0bba33cfce488bced640d4c313d2f1a7596fc176
Alexandre Savastano <xandy0210@gmail.com>
2017-06-06 23:24:36 -0300

    Ajeitando o cenario

---
+++ Assets/Scripts/CutSceneEvents.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFim : MonoBehaviour
+{
+
+    public void CameraAcabou()
+    {
+        CutsceneController.instancia.ProximaCamera();
+    }
+}
